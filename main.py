e8e7de56fe644ecd6af6b362dba324868d6f8057
adity <adityaburugu1234567@gmail.com>
2024-04-13 03:18:10 +0530

    Resonating freq calculation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def format_value(value):
     if abs(value) >= 1e6:
         formatted_value = round(value / 1e6,2)
@@ -12,6 +14,7 @@ def format_value(value):
     return f"{formatted_value}{unit}"
 
 def CE_Amplifier(VCC,Vce,Ic,B,Av):
+    Ic=eval(Ic)
     re = (26*10**-3)/(Ic)
     Rc = round(Av*re,2)
 
@@ -26,8 +29,8 @@ def CE_Amplifier(VCC,Vce,Ic,B,Av):
 
     R2_max = B/10*Re
 
-    R2 = int(input(f'''Select R2 Value less than {format_value(R2_max)}Ω:'''))
-    if R2>0 and R2<=R2_max:
+    R2 = eval(input(f'''Select R2 Value less than {format_value(R2_max)}Ω:'''))
+    if R2>0 and R2<=round(R2_max):
         R1 = ratio*R2
 
         Rc = format_value(Rc)
@@ -40,7 +43,7 @@ def CE_Amplifier(VCC,Vce,Ic,B,Av):
         return None,None,None,None
 
 def CC_Amplifier(VCC,Vce,Ic,B):
-
+    Ic=eval(Ic)
     Re = Vce/Ic
     VBE = 0.7  # fixed
     Base_Voltage = (Vce + VBE)
@@ -49,7 +52,7 @@ def CC_Amplifier(VCC,Vce,Ic,B):
 
     R2_max = B/10*Re
 
-    R2 = int(input(f'''Select R2 Value less than {format_value(R2_max)}Ω:'''))
+    R2 = eval(input(f'''Select R2 Value less than {format_value(R2_max)}Ω:'''))
     if R2>0 and R2<=R2_max:
         R1 = ratio*R2
         Rin = (R1 * R2) / (R1 + R2)
@@ -62,15 +65,18 @@ def CC_Amplifier(VCC,Vce,Ic,B):
     else:
         return None,None,None,None
 
-def CE_RC_Coupled_Amplifier():
-    pass
+def LP_HP_RC(R,C):
+    f=1/(2*np.pi*eval(R)*eval(C))
+    return f
+
 def CE():
+    print("Parameters of CE Amplifier")
     Vcc = float(input("Enter Vcc Value:"))
     Vce = float(input("Enter VceQ Value:"))
     if Vce > Vcc:
         print("Vce is greater than VCC")
         exit(0)
-    IcQ = float(input("Enter IcQ Value:"))
+    IcQ = input("Enter IcQ Value:")
     B = int(input("Enter Beta Value:"))
     Av = int(input("Enter required Gain:"))
     Rc, Re, R1, R2 = CE_Amplifier(Vcc, Vce, IcQ, B, Av)
@@ -80,12 +86,13 @@ def CE():
     print("Biasing Resistance2:", R2, "ohm")
 
 def CC():
+    print("Parameters of CC Amplifier")
     Vcc = float(input("Enter Vcc Value:"))
     Vce = float(input("Enter VceQ Value:"))
     if Vce > Vcc:
         print("Vce is greater than VCC")
         exit(0)
-    IcQ = float(input("Enter IcQ Value:"))
+    IcQ = input("Enter IcQ Value:")
     B = int(input("Enter Beta Value:"))
     Rin,Re, R1, R2 = CC_Amplifier(Vcc, Vce, IcQ, B)
     print("Impedence Matching Resistance:", Rin, "ohm")
@@ -93,13 +100,21 @@ def CC():
     print("Biasing Resistance1:", R1, "ohm")
     print("Biasing Resistance2:", R2, "ohm")
 
+def Freq():
+    R = input("Enter Resistance Value:")
+    C = input("Enter Capacitance Value:")
+    print("Resonating Frequency:",format_value(LP_HP_RC(R,C)))
+
+
 def main():
-    print("Select Amplifier\n1.CE Amplifier\n2.CC Amplifier")
+    print("Select Amplifier\n1.CE Amplifier\n2.CC Amplifier\n3.Resonating Freq of RC Circuit")
     choice = int(input("Enter your choice:"))
     if choice==1:
         CE()
     elif choice==2:
         CC()
+    elif choice==3:
+        Freq()
     else:
         print("Invalid Choice")
 
